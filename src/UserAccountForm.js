f50d1608f8c2aaad0638eea1e9a884ffd0bc208e
Yass <104535161+spooky662@users.noreply.github.com>
2024-11-29 22:09:59 -0300

    Update UserAccountForm.js

--- src/UserAccountForm.js
+++ src/UserAccountForm.js
@@ -24,7 +24,7 @@ const UserAccountForm = () => {
 
         try {
             console.log('Salvando dados');
-            const response = await axios.post('https://localhost:8080/users/novouser', formData,{
+            const response = await axios.post('http://localhost:8080/users/novouser', formData,{
                 headers:{
                     'Content-Type':'application/json'
                 }
